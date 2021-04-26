1bf3f96e198ed7c48e65b5513e11b4dfe045722c
Gerry M <waltermccone88@gmail.com>
2021-04-26 16:39:37 -0500

    created the server file and added the pre-built code to the repo

---
+++ server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+//require express and all of its dependencies
+const express = require('express');
+const mongoose = require("mongoose");
+
+const routes = require('./controllers');
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+//link the helpers file to handlebards
+
+
+//set up express
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+//set up routes
+app.use(routes);
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
