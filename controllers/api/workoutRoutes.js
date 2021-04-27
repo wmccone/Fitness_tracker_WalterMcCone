5c7d7c048f861f2400bdba3725fc56711b43c944
Walter M <waltermccone88@gmail.com>
2021-04-27 15:11:59 -0500

    created the file for api routes and included some base route structure

---
+++ controllers/api/workoutRoutes.js
@@ -0,0 +1,38 @@
+const router = require("express").Router();
+const {Workout, Exercise} = require("../models");
+
+// Route is going to get the last workout
+router.get('/', async (req, res) => {
+    try {
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+});
+
+// Route is going to add an exercise to the workout
+router.put('/:id', async (req, res) => {
+    try {
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+});
+
+// Route is going to create a new workout
+router.post('/', async (req, res) => {
+    try {
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+});
+
+//Route is going to get the data for workouts in range to add to chart.js
+router.get('/range', async (req, res) => {
+    try {
+
+    } catch(err){
+        res.status(500).json(err)
+    }
+});
